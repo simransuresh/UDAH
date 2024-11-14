389ce2e88299e37f60e834623940f349d87f2deb
Simran Suresh <simrans10496@gmail.com>
2024-11-14 13:36:15 +0100

    weights added for data point selection

--- objmap.py
+++ objmap.py
@@ -2,10 +2,9 @@ import numpy as np
 from helpers import *
 import math
 import random
-import csv
 import pandas as pd
-from datetime import datetime
 import time
+import csv
 
 L1 = 600
 phi1 = 1
@@ -13,8 +12,9 @@ L2 = 300
 phi2 = 0.4
 T = 60
 
-dp = pd.read_csv('data_points.csv')
-# dp = pd.read_csv('results/depth_500m.csv')  # TODO add DH into this csv and use this inside of filt dp csv
+# first all profiles from dynamic_height.py -> dh_2011_2018_deep.csv
+# dp = pd.read_csv('data_points.csv') -> only points within CAO
+dp = pd.read_csv('all_500m.csv')  # takes all >500m profiles for mapping grid points within CAO
 
 hydr_data = {
     (row['Latitude'], row['Longitude']): {
@@ -24,65 +24,71 @@ hydr_data = {
     }
     for _, row in dp.iterrows()
 }
-# print(hydr_data)
+
+# outlier elimination
+mean = np.nanmean([val['dh'] for val in hydr_data.values()])
+sd = np.nanstd([val['dh'] for val in hydr_data.values()])
+hydr_data = {key:val for key,val in hydr_data.items() if abs(val['dh'] - mean) < 2*sd}
+
+def find_weights(subset, latg, long, tg=None):
+    
+    D = D_mat(subset, target=(latg, long))
+    PV = PV_mat([ll[0] for ll in subset], [ll[1] for ll in subset], latg, long, depth_info)[:,0]
+    
+    weighting_func = (D/L1)**2 + (PV/phi1)**2
+    
+    # for stage 2 including t also
+    if tg is not None:  
+        Od_t = [hydr_data[item]['dt'] for item in subset]
+        t = tdiff(Od_t, tg)
+        weighting_func += (t/T)**2
+       
+    weight =  np.exp( -weighting_func )
+    weight = {ll: weight[idx] for idx, ll in enumerate(subset)}   # idx of subset should be same of D,PV,t mat idx
+
+    return weight
 
 def objmap(latg, long, tg):
 
     # print('#### ----- objective mapping started for the grid.....', latg, long, tg)
-    
-    ####### get data points and eliminate outliers more than 2SD
-    subset1 = [ll for ll, val in hydr_data.items() # take points within radius, within +-3yrs
-               if D_mat(ll, target=(latg, long)) <= L1 and \
-                tdiff(val['dt'], tg) <= 1095 and \
-                math.isnan(val['dh']) is False]    
-    Od1 = {ll:hydr_data[ll]['dh'] for ll in subset1}
-    Od1 = {ll:val for ll,val in Od1.items() 
-            if abs(val - np.mean(list(Od1.values()))) < 2*np.std(list(Od1.values()))}
-    subset1 = list(Od1.keys())
-    # print(len(subset1), time.time())
 
-    subset2 = [ll for ll, val in hydr_data.items() 
-               if D_mat(ll, target=(latg, long)) <= L2 and \
-                tdiff(val['dt'], tg) <= 1095 and \
-                math.isnan(val['dh']) is False]    
-    Od2 = {ll:hydr_data[ll]['dh'] for ll in subset2}
-    Od2 = {ll:val for ll,val in Od2.items() 
-            if abs(val - np.mean(list(Od2.values()))) < 2*np.std(list(Od2.values()))}
-    subset2 = list(Od2.keys())
-    # print(len(subset2), time.time())
-    # print(len(subset2))
+    ####### get data points 
+    subset1 = [ll for ll, val in hydr_data.items() # take points within L1 radius, within +-3yrs
+               if D_mat(ll, target=(latg, long)) <= L1 and tdiff(val['dt'], tg) <= 1095 and \
+                math.isnan(val['dh']) is False]   
+    # print(len(subset1))
     
     ###### pick data points for mapping
     if len(subset1) > 60:
         # pick random 1/3 points here   
         subset = random.sample(subset1, 20)
+        rem_subset = [point for point in subset1 if point not in subset]
+        
+        # 1/3 - from remaining get weights based on D, PV and sorted by highest to lowest and take first 20 points
+        w1 = find_weights(rem_subset, latg, long, tg=None)    
+        l1_subset = sorted(w1, key=w1.get, reverse=True)[0:20]
+        rem_subset1 = [point for point in subset1 if point not in l1_subset and point not in subset]
 
-        # # pick 1/3 points from L=600km radius with weight to closest points, sort to closest dist and select 1/3 points
-        dist1 = [(ll, D_mat(ll, target=(latg, long))) for ll in list(Od1.keys())]
-        sorted1 = sorted(dist1, key=lambda x: x[1])
-        sorted1 = [point for point, distance in sorted1 if point not in subset]
-        subset = subset + sorted1[0:20]
-
-        # # pick remaining 1/3 points from L=300km radius 
-        dist2 = [(ll, D_mat(ll, target=(latg, long))) for ll in list(Od2.keys())]   # TODO <60 points, if less available OK?
-        sorted2 = sorted(dist2, key=lambda x: x[1])
-        sorted2 = [point for point, distance in sorted2 if point not in subset]
-        subset = subset + sorted2[0:20]
-
-        # print(len(subset))
+        # 1/3 - same but used t weight also        
+        w2 = find_weights(rem_subset1, latg, long, tg=tg)
+        l2_subset = sorted(w2, key=w2.get, reverse=True)[0:20]
+        rem_subset2 = [point for point in subset1 if point not in l2_subset and point not in l1_subset and point not in subset]
+        
+        subset = list(set(subset+l1_subset+l2_subset))
+        
     else:
         subset = subset1
 
     # print('final subset', len(subset), time.time())
     
+    #### get value of dh from chosen subset
     Od = np.array([hydr_data[ll]['dh'] for ll in subset])
     n = len(Od)
     
     if len(Od)==0 or Od is None:
         return np.nan, np.nan
     
-    # print('######### STAGE 1 ############')
-    # print('Lphi---------', L1, phi1)
+    # print('######### STAGE 1 ############', L1, phi1)
 
     signal_variance = signal(Od, n)
     noise_variance = noise(subset, Od, n) 
@@ -104,45 +110,39 @@ def objmap(latg, long, tg):
     # print(Cdd.shape, Cdg.shape)
     
     Cdd += noise_variance * np.eye(Cdd.shape[0])
-    
     Od_mean = sum(Cdd@Od) / sum(sum(Cdd))
     
+    ### mapping procedure
     mapped_height = np.linalg.solve(Cdd, Od-Od_mean) 
-    
     Og1 = np.dot(Cdg.T, mapped_height) + Od_mean  
         
     Og1_error = np.sqrt(signal_variance - Cdg.T@Cdd@Cdg)
-    
     # print("Og1:", Og1, Og1_error) 
 
 
     ######## stage2
-    # print('######### STAGE 2 ############')
-    # print('Lphi---------', L2, phi2, T)
+    # print('######### STAGE 2 ############', L2, phi2, T)
     
-    Od_t = [hydr_data[item]['dt'] for item in subset]
     Od = Od-Og1
+    
     signal_variance = signal(Od, len(Od))
     noise_variance = noise(subset, Od, len(Od)) 
     
-    tdiff_dd = tdiff(Od_t) #days between dd matrix 
-    tdiff_dg = np.array([[d] for d in tdiff(Od_t, tg)])[:,0] #days between dg matrix
-    # print(tdiff_dd.shape, tdiff_dg.shape)
+    Od_t = [hydr_data[item]['dt'] for item in subset]
+    tdiff_dd = tdiff(Od_t) # days between dd matrix 
+    tdiff_dg = tdiff(Od_t, tg) #days between dg matrix
     
     Cdd = covar2(D_dd, PV_dd, tdiff_dd, signal_variance, L2, phi2, T)
     Cdg = covar2(D_dg, PV_dg, tdiff_dg, signal_variance, L2, phi2, T)
     # print(Cdd.shape, Cdg.shape)
     
     Cdd += noise_variance * np.eye(Cdd.shape[0])
-    
     Od_mean = sum(Cdd@Od) / sum(sum(Cdd))
 
     mapped_height = np.linalg.solve(Cdd, Od-Od_mean)  
-    
     Og2 = np.dot(Cdg.T, mapped_height) + Od_mean 
     
     Og2_error = np.sqrt(signal_variance - Cdg.T@Cdd@Cdg)
-    
     # print("Og2:", Og2, Og2_error) 
 
     # ###### final 
@@ -157,9 +157,25 @@ def objmap(latg, long, tg):
 # objmap(latg, long, tg)
 # objmap(87.5, 0, '2011-09-01')
 
-# gp = pd.read_csv('grid_points.csv')
-# t = '2011-01-01' #datetime(2011, 1, 1)
+gp = pd.read_csv('grid_points.csv')
+t = '2011-01-01' #datetime(2011, 1, 1)
 
-# for idx, row in gp.iterrows():
-#     (Og, Og_err) = objmap(row['Latitude'], row['Longitude'], '2011-01-01')
-#     print(idx, time.time(), Og, Og_err)
+# output_file = "results/grd_dh_2011_01_wnn.csv"  # another run with distance sorted poiints selected within L1 L2
+output_file = "results/grd_dh_2011_01_final.csv"  # another run with weight function inclusive
+
+fp = open(output_file, mode='w', newline='') 
+writer = csv.writer(fp)
+writer.writerow(["Datetime", "Latitude", "Longitude", "Depth", "Dynamic_Height", "DH_error"])    
+
+for idx, row in gp.iterrows():
+    lat, lon = row['Latitude'], row['Longitude']
+    (Og, Og_err) = objmap(lat, lon, t)
+    
+    print(idx, row['Latitude'], row['Longitude'], time.time(), Og, Og_err)
+    writer.writerow([t, lat, lon, depth_info[(lat, lon)]['depth'], Og, Og_err])
+    
+    # if idx==2:
+    #     break
+
+fp.close()
+print(f"Results written to {output_file}")
